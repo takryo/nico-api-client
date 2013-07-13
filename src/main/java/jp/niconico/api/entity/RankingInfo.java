cfff5a9c7180690808496c3e2ce769b1cbac099b
Ryou <Ryou@Ryou-PC>
2013-07-13 12:47:18 +0900

    ランキング取得のパラメータのEnumを作成

--- src/main/java/jp/niconico/api/entity/RankingInfo.java
+++ src/main/java/jp/niconico/api/entity/RankingInfo.java
@@ -1,6 +1,8 @@
 package jp.niconico.api.entity;
 
 import jp.niconico.api.exception.NiconicoException;
+import jp.niconico.api.method.constant.RankingKind;
+import jp.niconico.api.method.constant.RankingPeriod;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -9,123 +11,134 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RankingInfo {
-    public int rank;
+	public int rank;
 
-    public String rankKind;
+	public RankingKind rankKind;
 
-    public String period;
+	public RankingPeriod period;
 
-    public String title;
+	public String title;
 
-    public String link;
+	public String link;
 
-    public String pubDate;
+	public String pubDate;
 
-    public String date;
+	public String date;
 
-    public String description;
+	public String description;
 
-    public String thumbnailUrl;
+	public String thumbnailUrl;
 
-    public String length;
+	public String length;
 
-    public long viewCounter;
+	public long viewCounter;
 
-    public long commentNum;
+	public long commentNum;
 
-    public long mylistCounter;
+	public long mylistCounter;
 
-    public long viewCounterPeriod;
+	public long viewCounterPeriod;
 
-    public long commentNumPeriod;
+	public long commentNumPeriod;
 
-    public long mylistCounterPeriod;
+	public long mylistCounterPeriod;
 
-    public static List<RankingInfo> parse(String period, String rankKind, String xml) throws NiconicoException {
-        List<RankingInfo> list = new ArrayList<RankingInfo>();
+	public static List<RankingInfo> parse(RankingPeriod period,
+			RankingKind rankKind, String xml) throws NiconicoException {
+		List<RankingInfo> list = new ArrayList<RankingInfo>();
 
-        try {
-            ByteArrayInputStream in = new ByteArrayInputStream(xml.getBytes("UTF-8"));
-            Rss root = JAXB.unmarshal(in, Rss.class);
+		try {
+			ByteArrayInputStream in = new ByteArrayInputStream(
+					xml.getBytes("UTF-8"));
+			Rss root = JAXB.unmarshal(in, Rss.class);
 
-            for (int i = 0; i < root.channel.item.size(); i++) {
-                Rss.Channel.Item item = root.channel.item.get(i);
+			for (int i = 0; i < root.channel.item.size(); i++) {
+				Rss.Channel.Item item = root.channel.item.get(i);
 
-                RankingInfo info = new RankingInfo();
-                info.rank = i + 1;
-                info.period = period;
-                info.rankKind = rankKind;
-                info.title = item.title.substring(item.title.indexOf("：") + 1);
-                info.link = item.link;
-                info.pubDate = item.pubDate;
+				RankingInfo info = new RankingInfo();
+				info.rank = i + 1;
+				info.period = period;
+				info.rankKind = rankKind;
+				info.title = item.title.substring(item.title.indexOf("：") + 1);
+				info.link = item.link;
+				info.pubDate = item.pubDate;
 
-                String desc = item.description;
-                desc = desc.substring(desc.indexOf("src=\"") + "src=\"".length());
-                info.thumbnailUrl = desc.substring(0, desc.indexOf("\""));
+				String desc = item.description;
+				desc = desc.substring(desc.indexOf("src=\"")
+						+ "src=\"".length());
+				info.thumbnailUrl = desc.substring(0, desc.indexOf("\""));
 
-                desc = desc.substring(desc.indexOf("nico-description\">") + "nico-description\">".length());
-                info.description = desc.substring(0, desc.indexOf("</p>"));
+				desc = desc.substring(desc.indexOf("nico-description\">")
+						+ "nico-description\">".length());
+				info.description = desc.substring(0, desc.indexOf("</p>"));
 
-                desc = desc.substring(desc.indexOf("nico-info-length\">") + "nico-info-length\">".length());
-                info.length = desc.substring(0, desc.indexOf("</strong>"));
+				desc = desc.substring(desc.indexOf("nico-info-length\">")
+						+ "nico-info-length\">".length());
+				info.length = desc.substring(0, desc.indexOf("</strong>"));
 
-                desc = desc.substring(desc.indexOf("nico-info-date\">") + "nico-info-date\">".length());
-                info.date = desc.substring(0, desc.indexOf("</strong>"));
+				desc = desc.substring(desc.indexOf("nico-info-date\">")
+						+ "nico-info-date\">".length());
+				info.date = desc.substring(0, desc.indexOf("</strong>"));
 
-                desc = desc.substring(desc.indexOf("nico-info-total-view\">") + "nico-info-total-view\">".length());
-                info.viewCounter = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				desc = desc.substring(desc.indexOf("nico-info-total-view\">")
+						+ "nico-info-total-view\">".length());
+				info.viewCounter = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                desc = desc.substring(desc.indexOf("nico-info-total-res\">") + "nico-info-total-res\">".length());
-                info.commentNum = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				desc = desc.substring(desc.indexOf("nico-info-total-res\">")
+						+ "nico-info-total-res\">".length());
+				info.commentNum = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                desc = desc.substring(desc.indexOf("nico-info-total-mylist\">") + "nico-info-total-mylist\">".length());
-                info.mylistCounter = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				desc = desc.substring(desc.indexOf("nico-info-total-mylist\">")
+						+ "nico-info-total-mylist\">".length());
+				info.mylistCounter = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                String tmpPeriod;
-                if ("hourly".equals(period) || "daily".equals(period) || "weekly".equals(period) || "monthly".equals(period)) {
-                    tmpPeriod = period;
-                } else {
-                    tmpPeriod = "daily";
-                }
+				String classname = "nico-info-" + period + "-view\">";
+				desc = desc.substring(desc.indexOf(classname)
+						+ classname.length());
+				info.viewCounterPeriod = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                String classname = "nico-info-" + tmpPeriod + "-view\">";
-                desc = desc.substring(desc.indexOf(classname) + classname.length());
-                info.viewCounterPeriod = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				classname = "nico-info-" + period + "-res\">";
+				desc = desc.substring(desc.indexOf(classname)
+						+ classname.length());
+				info.commentNumPeriod = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                classname = "nico-info-" + tmpPeriod + "-res\">";
-                desc = desc.substring(desc.indexOf(classname) + classname.length());
-                info.commentNumPeriod = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				classname = "nico-info-" + period + "-mylist\">";
+				desc = desc.substring(desc.indexOf(classname)
+						+ classname.length());
+				info.mylistCounterPeriod = Long.parseLong(desc.substring(0,
+						desc.indexOf("</strong>")).replace(",", ""));
 
-                classname = "nico-info-" + tmpPeriod + "-mylist\">";
-                desc = desc.substring(desc.indexOf(classname) + classname.length());
-                info.mylistCounterPeriod = Long.parseLong(desc.substring(0, desc.indexOf("</strong>")).replace(",", ""));
+				list.add(info);
+			}
+		} catch (Exception e) {
+			throw new NiconicoException(e.getMessage());
+		}
 
-                list.add(info);
-            }
-        } catch (Exception e) {
-            throw new NiconicoException(e.getMessage());
-        }
+		return list;
+	}
 
-        return list;
-    }
+	@XmlRootElement
+	public static class Rss {
+		public Channel channel;
 
-    @XmlRootElement
-    public static class Rss {
-        public Channel channel;
+		public static class Channel {
+			public List<Item> item;
 
-        public static class Channel {
-            public List<Item> item;
+			public static class Item {
+				public String title;
 
-            public static class Item {
-                public String title;
+				public String link;
 
-                public String link;
+				public String pubDate;
 
-                public String pubDate;
-
-                public String description;
-            }
-        }
-    }
+				public String description;
+			}
+		}
+	}
 
 }
